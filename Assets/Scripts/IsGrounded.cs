c1adaefc80d612b334c23708d4a1db71f02ec219
Mantas <freker.mantas@gmail.com>
2018-12-06 15:37:46 +0000

    Initial commit

---
+++ Assets/Scripts/IsGrounded.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsGrounded : MonoBehaviour
+{
+
+    bool isgrounded = true;
+    public float jumpSpeed = 5f;
+    public Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (isgrounded == true)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                rb.AddForce(Vector3.up * jumpSpeed);
+            }
+        }
+    }
+
+    //make sure u replace "floor" with your gameobject name.on which player is standing
+    void OnCollisionEnter(Collision theCollision)
+    {
+        if (theCollision.gameObject.name == "Ground")
+        {
+            isgrounded = true;
+        }
+    }
+
+    //consider when character is jumping .. it will exit collision.
+    void OnCollisionExit(Collision theCollision)
+    {
+        if (theCollision.gameObject.name == "Ground")
+        {
+            isgrounded = false;
+        }
+    }
+}
---
+++ Assets/Scripts/MouseDis.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseDis : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+        Cursor.visible = false;
+    }
+}
